b11fe96c746d1c7b36bd035ee36664620673205e
sashort <95879519+sashort@users.noreply.github.com>
2022-04-30 22:01:20 -0400

    Update admin.py

--- welcome/admin.py
+++ welcome/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import PageView
+from .models import PageView, Book
 
 # Register your models here.
 
@@ -8,4 +8,4 @@ from .models import PageView
 class PageViewAdmin(admin.ModelAdmin):
     list_display = ['hostname', 'timestamp']
 
-admin.site.register(PageView, PageViewAdmin)
+admin.site.register(PageView, PageViewAdmin, Book)
