0c928fa5ded51b438042ed765d2a84a3bf0da032
sashort <95879519+sashort@users.noreply.github.com>
2022-04-30 21:07:37 -0400

    Update models.py

--- welcome/models.py
+++ welcome/models.py
@@ -5,3 +5,12 @@ from django.db import models
 class PageView(models.Model):
     hostname = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
+    
+class Book(models.Model):
+	rank = models.IntegerField()
+	title = models.CharField(max_length = 100)
+	author = models.CharField(max_length = 50)
+	year = models.IntegerField()
+	
+	def __str__(self):
+		return self.author
