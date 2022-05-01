0147fa53a1cdb30c9150542d1655e1e35b3f3b1a
sashort <95879519+sashort@users.noreply.github.com>
2022-04-30 21:06:46 -0400

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.http import HttpResponse
 
 from . import database
-from .models import PageView
+from .models import PageView, Book
 
 # Create your views here.
 
@@ -22,6 +22,18 @@ def index(request):
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
     return HttpResponse(PageView.objects.count())
+
+def book_list(request):
+	books = Book.objects.all()
+	context = {'book_list':books}
+	print(context)
+	return render(request, "books/book_list.html",context)
+
+
+def book_detail(request, id):
+	book = Book.objects.get(id = id)
+	context = {'book_detail': book}
+	return render(request, "books/book_detail.html",context)
 
 def hellr(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
