7fe5633e4d0c501b1244a47c3dcceff651b875a5
Andy Short <essayshort@gmail.com>
2022-05-01 00:03:59 -0400

    corrected indent error

--- books/migrations/0001_initial.py
+++ books/migrations/0001_initial.py
@@ -8,6 +8,16 @@ class Migration(migrations.Migration):
 
     dependencies = [
     ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PageView',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('hostname', models.CharField(max_length=32)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
         migrations.CreateModel(
             name='Book',
             fields=[
