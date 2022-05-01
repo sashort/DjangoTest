68194a80ec41f802ae24602bd4a0e252ed078eb6
sashort <95879519+sashort@users.noreply.github.com>
2022-04-30 21:50:37 -0400

    Update models.py

--- welcome/models.py
+++ welcome/models.py
@@ -7,10 +7,10 @@ class PageView(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     
 class Book(models.Model):
-	rank = models.IntegerField()
-	title = models.CharField(max_length = 100)
-	author = models.CharField(max_length = 50)
-	year = models.IntegerField()
-	
-	def __str__(self):
-		return self.author
+    rank = models.IntegerField()
+    title = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 50)
+    year = models.IntegerField()
+    	
+    def __str__(self):
+        return self.author
